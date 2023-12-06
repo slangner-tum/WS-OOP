74fd0515a2379266b9fb1d49df320d3aa4ade235
Sebastian <sebastian.langner@tum.de>
2023-12-06 14:17:33 +0100

    added some example functions

--- Examples.py
+++ Examples.py
@@ -18,6 +18,17 @@ def func_one_return(param1, param2):
 def func_multiple_return(param1, param2):
     return param1 * 2, param2 * 3
 
+
+def avg_bad(a, b):
+    return a + b / 2
+
+
+def avg_good(a, b):
+    return (a + b) / 2
+
+
+
+
 if __name__ == '__main__':
     print(f"return value of {func_no_return.__name__}: {func_no_return(5)}")
     print(f"return value of {func_one_return.__name__}: {func_one_return(5, 6)}")
